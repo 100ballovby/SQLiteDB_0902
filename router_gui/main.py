0237d7c326e98cc5d49174013df4a1748419849f
demidraksin <greatraksin@icloud.com>
2021-02-23 19:56:04 +0300

    Adding entry wigets🌜

--- router_gui/main.py
+++ router_gui/main.py
@@ -1,33 +1,78 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import Treeview
+from database import Database
+
+db = Database('shop', tn='routers')
 
 
-def populate_list(tree_view, db, hostname=''):
-    for i in tree_view.get_children():
-        tree_view.delete(i)
+def populate_list(hostname=''):
+    for i in router_tree_view.get_children():
+        router_tree_view.delete(i)
     for row in db.fetch(hostname):
-        tree_view.insert('', 'end', values=row)
+        router_tree_view.insert('', 'end', values=row)
 
 
-def populate_list2(tree_view, db, query='SELECT * FROM routers'):
-    for i in tree_view.get_children():
-        tree_view.delete(i)
+def populate_list2(query='SELECT * FROM routers'):
+    for i in router_tree_view.get_children():
+        router_tree_view.delete(i)
     for row in db.fetch2(query):
-        tree_view.insert('', 'end', values=row)
+        router_tree_view.insert('', 'end', values=row)
 
 
 def clear_text():
-    pass
+    brand_entry.delete(0, END)
+    hostname_entry.delete(0, END)
+    ram_entry.delete(0, END)
+    flash_entry.delete(0, END)
 
 
-def add_router(brand, host, ram, flash, db):
-    if brand.get() == '' or host.get() == '' or ram.get() == '' or flash.get() == '':
+def add_router():
+    if brand_text.get() == '' or hostname_text.get() == '' or ram_text.get() == '' or flash_text.get() == '':
         messagebox.showerror('Обязательные поля!', 'Пожалуйста, заполните все поля.')
         return
-    db.insert(host.get(), brand.get(), ram.get(), flash.get())
+    db.insert(hostname_text.get(), brand_text.get(), ram_text.get(), flash_text.get())
     clear_text()
     populate_list()
+
+
+def select_router(event):
+    try:
+        global selected_item
+        index = router_tree_view.selection()[0]
+        selected_item = router_tree_view.item(index)['values']
+        hostname_entry.delete(0, END)
+        hostname_entry.insert(END, selected_item[1])
+        brand_entry.delete(0, END)
+        brand_entry.insert(END, selected_item[2])
+        ram_entry.delete(0, END)
+        ram_entry.insert(END, selected_item[3])
+        flash_entry.delete(0, END)
+        flash_entry.insert(END, selected_item[4])
+    except IndexError:
+        messagebox.showwarning('Несуществующее значение!', 'Вы пытаетесь получить доступ к объекту, которого нет!')
+
+
+def delete_router():
+    db.remove(selected_item[0])
+    clear_text()
+    populate_list()
+
+
+def update_router():
+    db.update(selected_item[0], hostname_text.get(), brand_text.get(),
+              ram_text.get(), flash_text.get())
+    populate_list()
+
+
+def search_hostname():
+    hostname = hostname_search.get()
+    populate_list(hostname)
+
+
+def execute_query():
+    query = query_search.get()
+    populate_list2(query)
 
 app = Tk()
 frame_search = Frame(app)
@@ -79,5 +124,19 @@ flash_label = Label(frame_fields, text='Flash память', font=('bold', 14))
 flash_label.grid(row=1, column=2)
 flash_entry = Entry(frame_fields, textvariable=flash_text)
 flash_entry.grid(row=1, column=3)
+
+frame_router = Frame(app)
+frame_router.grid(row=4, column=0, columnspan=4,
+                  rowspan=6, pady=20, padx=20)
+columns = ['id', 'hostname', 'Brand', 'RAM', 'Flash']
+router_tree_view = Treeview(frame_router, columns=columns, show='headings')
+router_tree_view.column('id', width=30)
+
+for col in columns[1:]:
+    router_tree_view.column(col, width=120)
+    router_tree_view.heading(col, text=col)
+
+router_tree_view.bind('<<TreeviewSelect>>', select_router)
+router_tree_view.pack(side='left', fill='y')
 
 app.mainloop()
